a504488ed9b1f24801761574e2c0a6adf2bea6c3
PoulaHelmy <bolahelmy2016@gmail.com>
2023-12-08 15:54:44 +0200

    initial working version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/media-card/media-card.component').then((c) => c.MediaCardComponent),
+    pathMatch: 'full',
+  },
+];
